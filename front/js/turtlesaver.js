be393da394e3383dae551665bd87dfaed1e7b671
Emilie CLARY <emilie.clary@student.yncrea.fr>
2022-01-30 21:12:31 +0100

    Turtle changes

--- front/js/turtlesaver.js
+++ front/js/turtlesaver.js
@@ -19,12 +19,15 @@ var game = new Phaser.Game(config);
 
 var cursor;
 var ship;
+var turtle;
+
 
 function preload ()
 {
     this.load.image('background', '../img/turtle_saver/background.png');
     this.load.image('sky', '../img/turtle_saver/sky.png');
     this.load.image('ship', '../img/turtle_saver/fishing-ship.png');
+    this.load.image('turtle', '../img/turtle_saver/turtle.png');
 }
 
 
@@ -38,6 +41,32 @@ function create ()
     ship = this.physics.add.sprite(600, 90, 'ship');
     ship.setScale(0.15);
     ship.body.collideWorldBounds = true;
+
+    //class turtle extends Phaser.Physics.Arcade.Sprite {
+    //    constructor(x, y) {
+    //        this.x = x;
+    //        this.y = y;
+    //
+    //        this.turtle = this.scene.physics.add.sprite(x, y);
+    //}
+
+
+    for(let i = 0; i < 8; i++){
+        
+        let positionY = Phaser.Math.Between(150, this.game.config.height-50)
+
+        turtle = this.physics.add.sprite(0, positionY, 'turtle');
+        // turtle = this.physics.add.existing(new turtle(0), positionY)));
+        turtle.setScale(0.035);
+        turtle.setFlip(true, false);
+        turtle.body.collideWorldBounds = true;
+    }
+
+    // Il faut pouvoir différencier chaque instance de tortue pour pouvoir les contrôler séparémment (il n'y en a qu'une seule qui bouge pour l'instant)
+    // Je pense qu'il faut faire un constructeur (comme au dessus mais j'ai une erreur) à la manière des avions différenciables pour le projet
+
+
+
 }
 
 function update ()
@@ -52,5 +81,14 @@ function update ()
     }
     if(cursor.left.isUp && cursor.right.isUp){
         ship.setVelocityX(0);
+    }
+    
+    if(turtle.x >= 1159){
+        turtle.setVelocityX(-100);
+        turtle.setFlip(false, false);
+    }
+    else if(turtle.x <= 42){
+        turtle.setVelocityX(100);
+        turtle.setFlip(true, false);
     }
 }
